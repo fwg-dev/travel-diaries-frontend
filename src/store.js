4da2e9f4c437999e4889ebd9e95c61b41c6397de
fwg-dev <fwgicheh@gmail.com>
2020-11-01 21:35:35 -0800

    initialize store file

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import usersReducer from './reducers/users.js'
+import thunk from 'redux-thunk'
+
+const reducer = combineReducers({
+  user:  usersReducer
+ })
+ 
+ const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose; 
+ 
+ const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)) )
+ 
+
+ export default store
