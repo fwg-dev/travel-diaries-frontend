f7a8ad3d2fc463f27d24490eeb25b3cf5c7ee50b
fwg-dev <fwgicheh@gmail.com>
2020-11-01 21:33:36 -0800

    import redux store Provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Provider } from 'react-redux'
+import store from './store.js'
+
+
+ReactDOM.render(<Provider store={ store }><React.StrictMode><App /></React.StrictMode> </Provider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
