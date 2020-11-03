aa97e5d5cf27b1fab2ac888647f7104576a064c5
fwg-dev <fwgicheh@gmail.com>
2020-11-03 08:38:40 -0800

    import login form and currentUser reducers

--- src/store.js
+++ src/store.js
@@ -1,14 +1,22 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
 import thunk from 'redux-thunk'
 
+
+//just displaying different syntax options here(line 9, 10)
 const reducer = combineReducers({
-  user:  usersReducer
+  users:  usersReducer,
+  currentUser,
+  loginForm
  })
  
- const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose; 
+//  const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose; 
  
- const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)) )
+const composeEnhancer = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+ 
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)) )
  
 
  export default store
